512f969ac06dff349645ccada0af5ac529d97a7f
Milemotigo <global-config@mtgo3617@gmail.com>
2023-01-28 02:47:26 -0800

    m

---
+++ 0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include<stdio.h>
+
+/**
+ * print_list - a function that prints all the elements of a list_t list
+ * @h: pointer to the list
+ * Return: the number of nodes.
+ */
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+
+	while (h)
+	{
+		if (h->str !=NULL)
+		{
+		printf("[%d] %s\n", h->len, h->str);
+		}
+		else
+		{
+			printf("[0] (nil)\n");
+                }
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
